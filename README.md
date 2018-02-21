12baed7e288d29ca1bd167c5677287c4d03679ee
Kelp <kelp@androvideo.com>
2018-02-21 15:16:49 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gin-recovery
 The recovery of gin.
+
+
+```go
+import recovery
+
+router.Use(recovery.Recovery(recoveryHandler))
+
+func recoveryHandler(c *gin.Context, error *recovery.HttpError) {
+	if c.Request.Header.Get("Content-Type") == "application/json" {
+		c.AbortWithStatusJSON(error.Status, gin.H{
+			"error": error.Message,
+			"stack": strings.Split(string(errors.Stack(4)), "\n"),
+			"extra": error.Extra,
+		})
+	} else {
+		// render html error page.
+		c.HTML(500, "error.html", gin.H{
+			"title": "Error",
+			"err": error,
+		})
+		c.Abort()
+	}
+}
+```
---
+++ recovery.go
@@ -0,0 +1,120 @@
+package recovery
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http/httputil"
+	"net/http"
+	"runtime"
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	dunno     = []byte("???")
+	centerDot = []byte("·")
+	dot       = []byte(".")
+	slash     = []byte("/")
+)
+
+type HandlerFunc func(*gin.Context, *HttpError)
+
+type HttpError struct {
+	Status  int
+	Message string
+	Extra   interface{}
+}
+
+// Recovery middleware recovers from any panics and writes a 500 if there was one.
+func Recovery(handler HandlerFunc) gin.HandlerFunc {
+	out := gin.DefaultErrorWriter
+	var logger *log.Logger
+	if out != nil {
+		logger = log.New(out, "\n\n\x1b[31m", log.LstdFlags)
+	}
+
+	return func(c *gin.Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				var httpError HttpError
+				if err, ok := err.(HttpError); ok {
+					httpError = err
+				} else {
+					httpError = HttpError{
+						Status:  http.StatusInternalServerError,
+						Message: fmt.Sprintf("%s", err),
+					}
+				}
+				if logger != nil {
+					stack := Stack(3)
+					httprequest, _ := httputil.DumpRequest(c.Request, false)
+					logger.Printf("[Recovery] panic recovered:\n%s\nError: %s\n%s%s", string(httprequest), httpError.Message, stack, string([]byte{27, 91, 48, 109}))
+				}
+				handler(c, &httpError)
+			}
+		}()
+		c.Next()
+	}
+}
+
+// stack returns a nicely formated stack frame, skipping skip frames
+func Stack(skip int) []byte {
+	buf := new(bytes.Buffer) // the returned data
+	// As we loop, we open files and read them. These variables record the currently
+	// loaded file.
+	var lines [][]byte
+	var lastFile string
+	for i := skip; ; i++ { // Skip the expected number of frames
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		// Print this much at least.  If we can't find the source, it won't show.
+		fmt.Fprintf(buf, "%s:%d (0x%x)\n", file, line, pc)
+		if file != lastFile {
+			data, err := ioutil.ReadFile(file)
+			if err != nil {
+				continue
+			}
+			lines = bytes.Split(data, []byte{'\n'})
+			lastFile = file
+		}
+		fmt.Fprintf(buf, "\t%s: %s\n", function(pc), source(lines, line))
+	}
+	return buf.Bytes()
+}
+
+// function returns, if possible, the name of the function containing the PC.
+func function(pc uintptr) []byte {
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return dunno
+	}
+	name := []byte(fn.Name())
+	// The name includes the path name to the package, which is unnecessary
+	// since the file name is already included.  Plus, it has center dots.
+	// That is, we see
+	//	runtime/debug.*T·ptrmethod
+	// and want
+	//	*T.ptrmethod
+	// Also the package path might contains dot (e.g. code.google.com/...),
+	// so first eliminate the path prefix
+	if lastslash := bytes.LastIndex(name, slash); lastslash >= 0 {
+		name = name[lastslash+1:]
+	}
+	if period := bytes.Index(name, dot); period >= 0 {
+		name = name[period+1:]
+	}
+	name = bytes.Replace(name, centerDot, dot, -1)
+	return name
+}
+
+// source returns a space-trimmed slice of the n'th line.
+func source(lines [][]byte, n int) []byte {
+	n-- // in stack trace, lines are 1-indexed but our array is 0-indexed
+	if n < 0 || n >= len(lines) {
+		return dunno
+	}
+	return bytes.TrimSpace(lines[n])
+}
